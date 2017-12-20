80a26094a8af2aae06775f4d94429f5ac56bc44c
Batli Joselevitz <batlijoselevitz@Batlis-MacBook-Pro.local>
2017-12-19 18:34:33 -0600

    the rest of class activities

--- day2notes.js
+++ day2notes.js
@@ -3,3 +3,27 @@ express micro framework [what you learn here you can apply to these other framew
 play
 slim 3, laravel
 sinatra
+objectives:
+- create an express server
+- create a basic express GET route
+- know how to create an express post route
+- know what POST man is FOR
+- understand conceptually how to use ajax and get and POST data
+
+client server communication: GET
+  - CLIENT BROWSER:
+      - user visits/saved [GET REQUEST]...ie.typing something in a search bar or clicking on link
+      - user saves an article using api/headlines [POST REQUEST], ANYTHING not trivial should be a post request
+  - NODE BROWSWER:
+      - GET request: triggers the code in the server route
+      - POST Request: triggers the code in the server route, the server then sets the article as...
+
+
+
+
+
+
+
+
+
+*/
---
+++ server5.js
@@ -0,0 +1,4 @@
+make html file
+has bootstrap
+a css Page
+javascript user input to put name on table
